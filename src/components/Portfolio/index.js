1ab5ebfc80340be3b24064e4ac626775f77e759b
Aaron <aaronrobinett@gmail.com>
2022-01-23 00:52:59 -0800

    added projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,16 +6,32 @@ function Portfolio() {
     const [projects] = useState([
         {
             name: 'The Scroll Down',
-            photo: 'thescrolldown.png',
+            photo: 'thescrolldown.jpg',
             link: 'https://the-scroll-down.herokuapp.com/',
             github: 'https://github.com/aaronRobinett/the_scroll_down'
+        },
+        {
+            name: 'Bark',
+            photo: 'bark_v2.jpg',
+            link: 'https://bark-2.herokuapp.com/',
+            github: 'https://github.com/aaronRobinett/bark_2.0'
+        },
+        {
+            name: 'Budget Tracker',
+            photo: 'budget-tracker.jpg',
+            link: 'https://shrouded-badlands-98468.herokuapp.com/',
+            github: 'https://github.com/aaronRobinett/budget-tracker'
         }
     ])
     return (
         <section id="portfolio">
-            {projects.map((project) => (
-                <Project key={project.name} currentProject={project}></Project>
-            ))}
+            <div className="flex-row">
+                {projects.map((project) => (
+                    <Project key={project.name} currentProject={project}></Project>
+
+                ))}
+                {/* <Project currentProject={projects[0]}></Project> */}
+            </div>
         </section>
     );
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,15 +3,16 @@ import React from "react";
 function Project(props) {
     const { currentProject } = props;
     return (
-        <section>
-            <div>
-                <img src={require(`../../assets/${currentProject.photo}`).default}
-                    alt={currentProject.name} className="img-thumbnail mx-1" />
-            </div>
+        <div>
+            <img
+                src={require(`../../assets/${currentProject.photo}`)}
+                alt={currentProject.name}
+                className="mx-1"
+            />
             <h2>{`${currentProject.name}`}</h2>
             <a href={`${currentProject.link}`}>Link to deployed application.</a>
             <a href={`${currentProject.github}`}>Link to github repository.</a>
-        </section>
+        </div>
     )
 }
 
